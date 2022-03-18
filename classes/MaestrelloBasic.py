c1770aa4419f056cce6e31522cba4ced81526239
vlad boiko <mail@vladboiko.ru>
2022-03-19 00:42:58 +0300

    added basic class MaestrelloBasic

---
+++ classes/MaestrelloBasic.py
@@ -0,0 +1,29 @@
+import requests
+
+
+class MaestrelloBasic:
+    headers = {
+        "Accept": "application/json",
+        "Content-Type": "application/json",
+    }
+
+    api = "https://pizzamaestrello.com/api.php/p/v1/"
+
+    def __init__(self):
+        pass
+
+    def __validate(self, response, params):
+        if response.status_code == 200:
+            return response.json()
+
+            # data = response.json()
+            # if data.get('errors'):
+            #     print("status_code", response.status_code)
+            #     print("headers", self.headers)
+            #     print("params", params)
+            #     print("response", data)
+            # else:
+            #     return data
+
+        else:
+            print(response.status_code, response.text)
