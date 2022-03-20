23e9633539910637fbfa13fd597ec3714890a1a9
vlad boiko <mail@vladboiko.ru>
2022-03-20 21:20:15 +0300

    added test cases to class MaestrelloBasket

--- classes/MaestrelloBasket.py
+++ classes/MaestrelloBasket.py
@@ -1,3 +1,5 @@
+from copy import deepcopy
+
 import requests
 
 from classes.MaestrelloBasic import MaestrelloBasic
@@ -66,8 +68,11 @@ class MaestrelloBasket (MaestrelloBasic):
         if str(sku_id) in basket:
             item = basket.get(str(sku_id))
             quan = item["quantity"] - how_much_to_remove
-            item["quantity"] = quan if quan > 0 else 0
-            self._basket_update(str(sku_id), item)
+            if quan > 0:
+                item["quantity"] = quan
+                self._basket_update(str(sku_id), item)
+            else:
+                self.delete_product(sku_id)
 
     def get_basket(self):
         return self.__basket
@@ -87,6 +92,108 @@ class MaestrelloBasket (MaestrelloBasic):
         self.__basket.update({key: value})
 
     def _basket_delete(self, key: str):
-        self.__basket.pop(key, default=None)
+        self.__basket.pop(key, False)
 
 
+if __name__ == "__main__":
+
+    def show_title(text):
+        print(f"\n\n{text}\n")
+
+    mt_basket = MaestrelloBasket()
+    mt_basket.add_product(21, 7)
+    mt_basket.add_product(25, 10)
+    mt_basket.add_product_mod(21, 17)
+    mt_basket.add_product_mod(21, 18)
+    mt_basket.add_product_mod(25, 19)
+
+    # Добавить товар с ID 21 в корзину в количестве 5 единиц
+    show_title("Добавить товар в корзину в количестве 5 единиц")
+    basket = deepcopy(mt_basket)
+    product_id = 10
+    quantity = 3
+    print("Корзина", basket.get_basket())
+    basket.add_product(product_id, quantity)
+    print(f"Добавлен товара (id = {product_id}) в количестве {quantity} единиц")
+    print("Корзина", basket.get_basket())
+
+    # Добавить модификатор товару с ID 21 в корзине
+    show_title("Добавить модификатор товару в корзине")
+    basket = deepcopy(mt_basket)
+    product_id = 21
+    mod_id = 15
+    print("Корзина", basket.get_basket())
+    basket.add_product_mod(product_id, mod_id)
+    print(f"Добавлен модификатор (id = {mod_id}) для товара (id = {product_id})")
+    print("Корзина", basket.get_basket())
+
+    # Добавить количество товара в корзине
+    show_title("Добавить количество товару в корзине")
+    basket = deepcopy(mt_basket)
+    product_id = 21
+    add = 7
+    print("Корзина", basket.get_basket())
+    basket.add_quantity(product_id, add)
+    print(f"Добавлено {add} единиц товара (id = {product_id})")
+    print("Корзина", basket.get_basket())
+
+    # Убрать количество товара с ID 21 в корзине
+    show_title("Убрать количество товара в корзине")
+    basket = deepcopy(mt_basket)
+    product_id = 21
+    remove = 2
+    print("Корзина", basket.get_basket())
+    basket.remove_quantity(product_id, remove)
+    print(f"Убрано {remove} единиц товара (id = {product_id})")
+    print("Корзина", basket.get_basket())
+
+    # Убрать модификатор товара в корзине
+    show_title("Убрать модификатор товара в корзине")
+    basket = deepcopy(mt_basket)
+    product_id = 21
+    mod_id = 17
+    print("Корзина", basket.get_basket())
+    basket.delete_product_mod(product_id, mod_id)
+    print(f"Удален модификатов (id = {mod_id}) товара (id = {product_id})")
+    print("Корзина", basket.get_basket())
+
+    # Убрать все модификаторы товара в корзине
+    show_title("Убрать все модификаторы товара в корзине")
+    basket = deepcopy(mt_basket)
+    product_id = 21
+    print("Корзина", basket.get_basket())
+    basket.delete_product_mods(product_id)
+    print(f"Удалены все модификаторы товара (id = {product_id})")
+    print("Корзина", basket.get_basket())
+
+    # Получить корзину в формате для заказа
+    show_title("Получить корзину в формате для заказа")
+    basket = deepcopy(mt_basket)
+    print(basket.get_basket_for_order())
+
+    # Удалить товар из корзины
+    show_title("Удалить товар из корзины")
+    basket = deepcopy(mt_basket)
+    product_id = 25
+    print("Корзина", basket.get_basket())
+    basket.delete_product(product_id)
+    print(f"Удален товар (id = {product_id})")
+    print("Корзина", basket.get_basket())
+
+    # Убрать заведомо большое количество одного товара (будет удален из корзины)
+    show_title("Убрать заведомо большое количество одного товара (будет удален из корзины)")
+    basket = deepcopy(mt_basket)
+    product_id = 21
+    quantity = 1000
+    print("Корзина", basket.get_basket())
+    basket.remove_quantity(product_id, quantity)
+    print(f"Убрано {quantity} единиц товара (id = {product_id})")
+    print("Корзина", basket.get_basket())
+
+    # Очистить корзину
+    show_title("Очистить корзину")
+    basket = deepcopy(mt_basket)
+    print("Корзина", basket.get_basket())
+    basket.clear_basket()
+    print(f"Корзина очищена")
+    print("Корзина", basket.get_basket())
