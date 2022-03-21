b3dacdb90b918d7edc4b029cc30d10b50fbbf417
vlad boiko <mail@vladboiko.ru>
2022-03-21 17:16:28 +0300

    added docs to MaestrelloBasket

--- classes/MaestrelloBasket.py
+++ classes/MaestrelloBasket.py
@@ -9,6 +9,13 @@ class MaestrelloBasket (MaestrelloBasic):
         self.__basket = dict()
 
     def add_product(self, sku_id: int, quantity: int = 1):
+        """
+        Добавить товар в корзину
+
+        :param sku_id: SKU ID товара
+        :param (не обязательный) quantity: количество единиц товара
+        """
+
         new_item = {
             "sku_id": sku_id,
             "quantity": quantity,
@@ -18,6 +25,13 @@ class MaestrelloBasket (MaestrelloBasic):
         self._basket_update(str(sku_id), new_item)
 
     def add_product_mod(self, sku_id: int, mod_id: int):
+        """
+        Добавить модификатор для товара в корзину
+
+        :param sku_id: SKU ID товара
+        :param mod_id: SKU ID модификатора товара
+        """
+
         basket = self.get_basket()
         if str(sku_id) in basket:
             item = basket.get(str(sku_id))
@@ -28,9 +42,21 @@ class MaestrelloBasket (MaestrelloBasic):
             self._basket_update(str(sku_id), item)
 
     def delete_product(self, sku_id: int):
+        """
+        Удалить товар из корзины
+
+        :param sku_id: SKU ID товара
+        """
         self._basket_delete(str(sku_id))
 
     def delete_product_mod(self, sku_id: int, mod_id: int):
+        """
+        Удалить модификатор для товара в корзине
+
+        :param sku_id: SKU ID товара
+        :param mod_id: SKU ID модификатора товара
+        """
+
         basket = self.get_basket()
         if str(sku_id) in basket:
             item = basket.get(str(sku_id))
@@ -44,6 +70,12 @@ class MaestrelloBasket (MaestrelloBasic):
             self._basket_update(str(sku_id), item)
 
     def delete_product_mods(self, sku_id: int):
+        """
+        Удалить все модификаторы для товара в корзине
+
+        :param sku_id: SKU ID товара
+        """
+
         basket = self.get_basket()
         if str(sku_id) in basket:
             item = basket.get(str(sku_id))
@@ -51,6 +83,13 @@ class MaestrelloBasket (MaestrelloBasic):
             self._basket_update(str(sku_id), item)
 
     def add_quantity(self, sku_id: int, how_much_to_add: int = 0):
+        """
+        Добавить количество товара в корзине
+
+        :param sku_id: SKU ID товара
+        :param how_much_to_add: сколько единиц товара добавить
+        """
+
         basket = self.get_basket()
 
         if str(sku_id) in basket:
@@ -61,6 +100,13 @@ class MaestrelloBasket (MaestrelloBasic):
             self.add_product(sku_id, how_much_to_add)
 
     def remove_quantity(self, sku_id: int, how_much_to_remove: int = 0):
+        """
+        Убрать количество товара в корзине
+
+        :param sku_id: SKU ID товара
+        :param how_much_to_remove: сколько единиц товара убрать
+        """
+
         basket = self.get_basket()
 
         if str(sku_id) in basket:
@@ -73,9 +119,54 @@ class MaestrelloBasket (MaestrelloBasic):
                 self.delete_product(sku_id)
 
     def get_basket(self):
+        """
+        Получить список товаров в корзине
+
+        :return
+        Список товаров в корзине
+        {
+            "63": {
+                "sku_id": "63",
+                "quantity": "3",
+                "services": [
+                    {
+                        "service_variant_id": "15"
+                    }
+                ]
+            },
+            "58": {
+                "sku_id": "58",
+                "quantity": "7",
+                "services": []
+            },
+        }
+        """
         return self.__basket
 
     def get_basket_for_order(self):
+        """
+        Получить список товаров в корзине в формате для оформления заказа
+
+        :return
+        Список товаров в корзине
+        [
+            {
+                "sku_id": "63",
+                "quantity": "3",
+                "services": [
+                    {
+                        "service_variant_id": "15"
+                    }
+                ]
+            },
+            {
+                "sku_id": "58",
+                "quantity": "7",
+                "services": []
+            },
+        ]
+        """
+
         basket = self.get_basket()
         order_basket = []
         for item in basket.values():
@@ -84,12 +175,26 @@ class MaestrelloBasket (MaestrelloBasic):
         return order_basket
 
     def clear_basket(self):
+        """
+        Очистить корзину
+        """
         self.__basket.clear()
 
     def _basket_update(self, key: str, value):
+        """
+        Обновить товар в спискок товаров корзины
+
+        :param key: id обновляемого товара
+        :param value: новое значение товара
+        """
         self.__basket.update({key: value})
 
     def _basket_delete(self, key: str):
+        """
+        Удалить товар из списка товаров корзины
+
+        :param key: id удаляемого товара
+        """
         self.__basket.pop(key, False)
 
 
